f9457a583d765ea5d8cd580cea798669382e566e
Noelther <noelther@proton.me>
2025-08-17 16:56:49 +0300

    initial commit

---
+++ src/main/kotlin/com/example/art_manager/ArtManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.art_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArtManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/art_manager/ArtManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.art_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArtManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
